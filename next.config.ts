7889bfe22de811f10665fc52eef0fff9790956e3
sridhar7670 <sridharnani080@gmail.com>
2025-07-05 19:39:48 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // reactStrictMode: false,  this is causing clg statments run 4 times
 };
 
 export default nextConfig;
---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+export default function Product(){
+    return (
+        <div>
+            <h1>products details</h1>
+        <ul>
+            <li>prod 1</li>
+            <li>prod 2</li>
+            <li>prod 3</li>
+        </ul>
+        </div>
+    )
+}
