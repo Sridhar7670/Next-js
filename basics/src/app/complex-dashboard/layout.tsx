22206faa27998e6c77530a38301ec45393777157
sridhar7670 <sridharnani080@gmail.com>
2025-07-11 12:37:50 +0530

    parrel routing

--- basics/src/app/complex-dashboard/layout.tsx
+++ basics/src/app/complex-dashboard/layout.tsx
@@ -1,14 +1,25 @@
 export default function ComplexDashboardLayout({
     children,
+    userMetrics,
+    revenue,
+    notifications
 }:{
-    children:React.ReactNode
+    children:React.ReactNode;
+    userMetrics:React.ReactNode;
+    revenue:React.ReactNode;
+    notifications:React.ReactNode;
+    
 }){
     return (
         <>
         <div>{children}</div>
-        <UserAnalytics/>
-        <RevenueMetrics/>
-        <Notifications/>
+        <div style={{display:"flex"}}>
+            <div style={{display:"flex",flexDirection:"column"}}>
+                <div>{revenue}</div>
+                <div>{userMetrics}</div>
+            </div>
+            <div style={{display:"flex",flex:1}}>{notifications}</div>
+        </div>
         </>
     )
 }
