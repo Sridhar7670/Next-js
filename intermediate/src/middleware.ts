20f7cd398c96c2d1d2c7548b3e123e1d7bb59999
sridhar7670 <sridharnani080@gmail.com>
2025-07-16 13:24:00 +0530

    added a readme.md

--- intermediate/src/middleware.ts
+++ intermediate/src/middleware.ts
@@ -1,18 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request:NextRequest){
+// export function middleware(request:NextRequest){
     // return NextResponse.redirect(new URL("/",request.url));
 
-    if(request.nextUrl.pathname==="/profiledata"){
-        return NextResponse.redirect(new URL("/time",request.nextUrl))
-    }
+    // if(request.nextUrl.pathname==="/profiledata"){
+    //     return NextResponse.redirect(new URL("/time",request.nextUrl))
+    // }
 // NextRequest.redirect - to completetly navigate to some other page 
-    if(request.nextUrl.pathname==="/profiledata"){
-        return NextResponse.rewrite(new URL("/time",request.nextUrl))
-    }
+    // if(request.nextUrl.pathname==="/profiledata"){
+    //     return NextResponse.rewrite(new URL("/time",request.nextUrl))
+    // }
 // Nextresponse.rewrite  to keep url in browser and change the serving conetent 
-}
+// }
 
 // export const config={
 //     matcher:"/profile",
 // };
+
+export function middleware(request:NextRequest){
+    const response = NextResponse.next();
+    const themePreference=request.cookies.get("theme");
+    if(!themePreference){
+        response.cookies.set("theme","dragon");
+    }
+
+    response.headers.set("custom","thisHEADERIscustome")
+    return response;
+}
